2b15833955f3704e711a7ee6644e08a6796ba4e2
Ashanti <shamarkey03@yahoo.com>
2026-08-24 01:37:23 -0500

    check the public view page

--- script.js
+++ script.js
@@ -6,13 +6,27 @@ console.log(headline);
 let first = document.getElementById("first-1");
 let second = document.getElementById("second-2");
 let third = document.getElementById("third-3");
+let pick = document.getElementById("pick-1");
 
 first.addEventListener("click", function() {
 	first.textContent = "Correct!";
+	pick.textContent="You picked one";
+	
 });	
 second.addEventListener("click", function() {
 	second.textContent = "Cold!";
+	pick.textContent="You picked one";
 });
 third.addEventListener("click", function() {
 	third.textContent = "Getting warmer!";
+	pick.textContent="You picked one";
 });
+
+
+
+pick.addEventListener("click", function() {
+	pick.textContent="You picked one";
+});
+	
+localStorage.setItem("visits", 5);
+let saved = localStorage.getItem("visits");
