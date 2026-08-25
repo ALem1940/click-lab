ae85f268193af5acb5ae044eac2650f634ab013c
Ashanti <shamarkey03@yahoo.com>
2026-08-25 15:56:31 -0500

    Add a cover/background image

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ console.log(headline);
 let first = document.getElementById("first-1");
 let second = document.getElementById("second-1");
 let third = document.getElementById("third-1");
+let firstTwo = document.getElementById("first-2");
+let secondTwo = document.getElementById("second-2");
+let thirdTwo = document.getElementById("third-2");
+let firstThird = document.getElementById("first-3");
+let secondThird = document.getElementById("second-3");
+let thirdThird = document.getElementById("third-3");
+
 let pick = document.getElementById("pick-1");
 
 first.addEventListener("click", function() {
@@ -21,7 +28,32 @@ third.addEventListener("click", function() {
 	third.textContent = "Getting warmer!";
 	pick.textContent="You picked one";
 });
-
+firstTwo.addEventListener("click", function() {
+	firstTwo.textContent = "Correct!";
+	pick.textContent="You picked one";
+	
+});	
+secondTwo.addEventListener("click", function() {
+	secondTwo.textContent = "Cold!";
+	pick.textContent="You picked one";
+});
+thirdTwo.addEventListener("click", function() {
+	thirdTwo.textContent = "Getting warmer!";
+	pick.textContent="You picked one";
+});
+firstThird.addEventListener("click", function() {
+	firstThird.textContent = "Correct!";
+	pick.textContent="You picked one";
+	
+});	
+secondThird.addEventListener("click", function() {
+	secondThird.textContent = "Cold!";
+	pick.textContent="You picked one";
+});
+thirdThird.addEventListener("click", function() {
+	thirdThird.textContent = "Getting warmer!";
+	pick.textContent="You picked one";
+});
 
 
 pick.addEventListener("click", function() {
